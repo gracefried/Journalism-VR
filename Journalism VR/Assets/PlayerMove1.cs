44b4da7b0d06c9f6491906aa47f51fd327d1a053
chetaylo844 <churro555sastre@ufl.edu>
2018-03-19 17:33:48 -0400

    Created basic tablet with AI
    Basic mockup of tablet + AI that sends the voice prompt options to the tablet, but removes them once player is out of range

--- Journalism VR/Assets/PlayerMove1.cs
+++ Journalism VR/Assets/PlayerMove1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove1 : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     CharacterController charControl;
     public float walkSpeed;
---
+++ Journalism VR/Assets/Scripts/OptionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionController : MonoBehaviour {
+
+    public string OptionA;
+    public string OptionB;
+    public string OptionC;
+
+
+	// Use this for initialization
+	void Start () {
+        /*OptionA = "Option A";
+        OptionB = "Option B";
+        OptionC = "Option C";*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
