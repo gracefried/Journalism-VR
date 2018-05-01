2aa41a8983b20ac1586d917b8009e2d13380e9cc
chetaylo844 <churro555sastre@ufl.edu>
2018-05-01 08:31:36 -0400

    Update to scene transitions
    Scene transition stuff

--- Journalism VR/Assets/Scripts/SceneControl.cs
+++ Journalism VR/Assets/Scripts/SceneControl.cs
@@ -7,21 +7,31 @@ public class SceneControl : MonoBehaviour {
     public string nextScene;
     public GameObject black;
     public float delay;
+    public float gameTimer;
     bool end;
 
 	// Use this for initialization
 	void Start () {
         end = false;
+        gameTimer = 120;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(end == true) { delay -= Time.deltaTime; }
-	}
+        if(SceneManager.GetActiveScene().name == "Start") { if (Input.anyKeyDown) { transition(); } }
+        if (SceneManager.GetActiveScene().name == "End") { if (Input.anyKeyDown) { Application.Quit(); } }
+        if(SceneManager.GetActiveScene().name == "ProtestScene1")
+        {
+            if(gameTimer > 0) { gameTimer -= Time.deltaTime; }
+            else { transition(); }
+        }
+    }
 
     public void transition()
     {
         end = true;
         black.GetComponent<Animator>().SetTrigger("fade");
+        //if(delay == 0){SceneManager.LoadScene(nextScene);}
     }
 }
